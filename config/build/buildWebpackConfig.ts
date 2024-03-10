c5182c544afb6508df3b9b42076f8531d9409896
Irina <tymanira@mail.ru>
2024-03-10 10:46:20 +0500

    feat: init webpack build

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import { Configuration } from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+    const {paths, mode, isDev} = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        plugins:  buildPlugins(options),
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
+}
